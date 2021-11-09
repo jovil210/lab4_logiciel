f3ebb994adcf29f09721257d9af5a5e9868540e9
Joel <joelv3120@hotmail.com>
2021-11-09 17:35:51 -0500

    env init

--- server/main.py
+++ server/main.py
@@ -3,6 +3,6 @@ from socketserver import TCPServer
 
 
 if __name__ == '__main__':
-    with TCPServer(('', 8081), Lab4HTTPRequestHandler) as tcp_server:
-        print('Serving on http://localhost:8081')
+    with TCPServer(('', 8080), Lab4HTTPRequestHandler) as tcp_server:
+        print('Serving on http://localhost:8080')
         tcp_server.serve_forever()
